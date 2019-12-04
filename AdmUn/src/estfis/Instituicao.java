4f2e68c18a3b639bd874cb99f9b68e6e4cb39581
viniciused26 <viniciusoliv26@gmail.com>
2019-12-04 15:14:50 -0300

    Adicionando método de cadastro de alunos à instituições.

--- AdmUn/src/estfis/Instituicao.java
+++ AdmUn/src/estfis/Instituicao.java
@@ -9,6 +9,7 @@ public class Instituicao extends Estrutura {
 		private String uf;
 		Campus cp[];
 		private List<Professor> profsCadastrados = new ArrayList<Professor>(); //array list dos professores cadastrados nessa instituição
+		private List<Aluno> alsCadastrados = new ArrayList<Aluno>(); //array list dos alunos cadastrados nessa disciplina
 		
 		public Instituicao (String nom, String uf) {
 			this.nome = nom;
@@ -22,14 +23,31 @@ public class Instituicao extends Estrutura {
 			profsCadastrados.add(professor); //adiciona o novo objeto criado no array list dos professores cadastrados nessa disciplina
 		}
 		
-		//método para retornar os professores cadastrados na instituição;
+		//método para retornar os professores cadastrados na instituição
 		public void profCads() {
 			int qntProfessoresCadastrados = profsCadastrados.size(); //encontra o tamanho atual do array list dos professores cadastrados nessa instituição
-			int i; //contados
+			int i; //contador
 			
 			for(i = 0; i < qntProfessoresCadastrados; i++) {
 				System.out.println(profsCadastrados.get(i).getNome()); //retorna o nome do professor
 				System.out.println(profsCadastrados.get(i).getCadastro()); //retorna o cadastro do professor
+			}
+		}
+		
+		//método para cadastrado de alunos
+		public void cadAls(String nome, int matricula) {
+			Aluno aluno = new Aluno(nome, matricula); //cria um novo objeto da classe Aluno com os parâmetros
+			alsCadastrados.add(aluno); //adiciona o novo objeto criado no array list dos alunos cadastrados nessa disciplina
+		}
+		
+		//método para retornar os alunos cadastrados na instituição
+		public void alsCads() {
+			int qntAlunosCadastrados = alsCadastrados.size(); //encontra o tamanho atual do array list dos professores
+			int i; //contador
+			
+			for(i = 0; i < qntAlunosCadastrados; i++) {
+				System.out.println(alsCadastrados.get(i).getNome()); //retorna o nome do aluno
+				System.out.println(alsCadsatrados.get(i).getMatricula()); //retorna a matrícula do aluno
 			}
 		}
 }
