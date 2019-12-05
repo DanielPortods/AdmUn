353b78d95e8f7264c51454479b1b7a72289845bd
Gabriel Bonifacio <gabrielbpn0802@gmail.com>
2019-12-05 17:50:07 -0200

    Exceptions

--- AdmUn/src/excecoes/DisciplinaNaoInformadaException.java
+++ AdmUn/src/excecoes/DisciplinaNaoInformadaException.java
@@ -1,5 +1,16 @@
 package excecoes;
+import javax.swing.JOptionPane;
 
-public class DisciplinaNaoInformadaException {
-
+public class DisciplinaNaoInformadaException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	private String disc;
+	
+	public DisciplinaNaoInformadaException (String d) {
+		this.disc = d;
+	}
+	
+	public void msg () {
+		JOptionPane.showMessageDialog(null, "Insira a disciplina " + disc);
+	}
 }
--- AdmUn/src/excecoes/ProfessorNaoAtribuidoException.java
+++ AdmUn/src/excecoes/ProfessorNaoAtribuidoException.java
@@ -1,7 +1,17 @@
 package excecoes;
+import javax.swing.JOptionPane;
 
 public class ProfessorNaoAtribuidoException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+private String pro;
+	
+	public ProfessorNaoAtribuidoException (String p) {
+		this.pro = p;
+	}
+	
+	public void msg () {
+		JOptionPane.showMessageDialog(null, "Insira o professor " + pro);
+	}
 }
