b6cba2ad11b565e9fe8905915b76c6a596c4b4c8
Gabriel Bonifacio <gabrielbpn0802@gmail.com>
2019-12-05 17:50:07 -0200

    Exceptions

--- AdmUn/src/excecoes/TipoDeAulaNaoAtribuidoException.java
+++ AdmUn/src/excecoes/TipoDeAulaNaoAtribuidoException.java
@@ -1,5 +1,17 @@
 package excecoes;
+import javax.swing.JOptionPane;
 
 public class TipoDeAulaNaoAtribuidoException {
 
-}
+	private static final long serialVersionUID = 1L;
+
+	private String aula;
+		
+		public TipoDeAulaNaoAtribuidoException (String t) {
+			this.aula = t;
+		}
+		
+		public void msg () {
+			JOptionPane.showMessageDialog(null, "Insira o tipo de aula " + aula);
+		}
+}	
