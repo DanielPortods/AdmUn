714fe35454f25e7470854119f75c8fc344b0f02f
Gabriel Bonifacio <gabrielbpn0802@gmail.com>
2019-12-06 01:47:39 -0200

    arrumando exceptions

--- AdmUn/src/excecoes/ProfessorNaoAtribuidoException.java
+++ AdmUn/src/excecoes/ProfessorNaoAtribuidoException.java
@@ -2,16 +2,8 @@ package excecoes;
 import javax.swing.JOptionPane;
 
 public class ProfessorNaoAtribuidoException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-private String pro;
-	
-	public ProfessorNaoAtribuidoException (String p) {
-		this.pro = p;
-	}
 	
 	public void msg () {
-		JOptionPane.showMessageDialog(null, "Insira o professor " + pro);
+		JOptionPane.showMessageDialog(null, "Professor não atribuído!");
 	}
 }
