40fae20a3cefbf065fd8e9f4b676fc308e98470d
Gabriel Bonifacio <gabrielbpn0802@gmail.com>
2019-12-09 03:51:53 -0200

    loggeral

--- AdmUn/src/excecoes/LogGeralException.java
+++ AdmUn/src/excecoes/LogGeralException.java
@@ -3,17 +3,25 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class LogGeralException extends Exception {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;	
+	private PrintWriter out;
 	
-	public void msg () throws IOException {
-		
-	LogGeralException erros = new LogGeralException();
-	ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(new File("logErros.txt")));
-	o.writeObject(erros);
-	o.close();
-	
+	public LogGeralException() {
+		try {
+			out = new PrintWriter(new FileWriter("Erros.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		if (out != null) {
+                        out.flush();
+			out.close();
+		}
 	}
 }
