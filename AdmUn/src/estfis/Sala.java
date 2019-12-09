b219b99d9e5bdcbe7be0a8a4affcdee6afc43d71
viniciused26 <viniciusoliv26@gmail.com>
2019-12-09 02:13:06 -0300

    Adicionando cálculo da taxa de ocupação do prédio.

--- AdmUn/src/estfis/Sala.java
+++ AdmUn/src/estfis/Sala.java
@@ -2,12 +2,14 @@ package estfis;
 
 import java.util.ArrayList;
 
+import abs.Ocupacao;
 import excecoes.CampoEmBrancoException;
 import excecoes.OpcaoInvalidaException;
 
 public abstract class Sala extends Estrutura {
 	protected String categoria, tipo;
 	protected int andar, capacidade;
+	public Ocupacao ocup;
 	
 	public String gettp() {
 		return this.tipo;
